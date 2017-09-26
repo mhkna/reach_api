a06844413bcb8cd4b0a6ad5923a6f468041d9403
Michael <michkna3@gmail.com>
2017-09-26 17:05:38 -0500

    added create, update and delete routes for posts

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -7,8 +7,38 @@ module Api
       end
 
       def show
-        post = Article.find(params[:id])
+        post = Post.find(params[:id])
         render json: {status: 'SUCCESS', message: 'loaded post', data: post}, status: :ok
+      end
+
+      def create
+        post = Post.new(post_params)
+        if post.save
+          render json: {status: 'SUCCESS', message: 'saved post', data: post}, status: :ok
+        else
+          render json: {status: 'ERROR', message: 'post not saved', data: post.errors}, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        post = Post.find(params[:id])
+        post.destroy
+        render json: {status: 'SUCCESS', message: 'deleted post', data: post}, status: :ok
+      end
+
+      def update
+        post = Post.find(params[:id])
+        if post.update_attributes(post_params)
+          render json: {status: 'SUCCESS', message: 'updated post', data: post}, status: :ok
+        else
+          render json: {status: 'ERROR', message: 'post not updated', data: post.errors}, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def post_params
+        params.permit(:content)
       end
     end
   end
